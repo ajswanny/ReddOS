b4d55c4ce99ad36f9a3eb08f8df6fb6a0008c561
ajsv <alexanderjswanson@icloud.com>
2020-01-28 12:46:48 -0500

    Initial Commit

---
+++ ReddOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ReddOS
+//
+//  Created by Alexander Swanson on 1/28/20.
+//  Copyright © 2020 SandPeople. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ReddOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ReddOS
+//
+//  Created by Alexander Swanson on 1/28/20.
+//  Copyright © 2020 SandPeople. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
