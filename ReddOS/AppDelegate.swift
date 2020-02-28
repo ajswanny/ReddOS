a502b383eaada879e6918f29b337aae05cefc763
ajsv <alexanderjswanson@icloud.com>
2020-02-28 01:03:59 -0500

    Implemented sample data

--- ReddOS/AppDelegate.swift
+++ ReddOS/AppDelegate.swift
@@ -11,6 +11,8 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var sampleData = SampleData()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
