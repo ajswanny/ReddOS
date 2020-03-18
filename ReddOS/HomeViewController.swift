33d730f5fe29693ad77a650be92d566001331cb3
JJ <jjthissell@gmail.com>
2020-03-18 10:28:01 -0400

    initial tableview and detail view

---
+++ ReddOS/HomeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  FirstViewController.swift
+//  ReddOS
+//
+//  Created by Alexander Swanson on 1/28/20.
+//  Copyright © 2020 SandPeople. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+    
+    var hotSubmissions: [Submission]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return hotSubmissions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Hot thread", for: indexPath)
+        let submisson = hotSubmissions[indexPath.row]
+        cell.textLabel!.text = submisson.title
+       // cell.textLabel?.text = submisson.subreddit
+        return cell
+
+}
+//
+}
+
+class HomeViewCell: UITableViewCell {
+    @IBOutlet weak var hotThreadImage: UIImageView!
+    @IBOutlet weak var hotThreadTitle: UILabel!
+    @IBOutlet weak var hotThreadSubReddit: UILabel!
+    @IBOutlet weak var upVote: UIButton!
+    @IBOutlet weak var downVote: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
+
