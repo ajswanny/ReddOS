f4daf391c7fa57543e261b4f1e38152a5e2a1f29
JJ <jjthissell@gmail.com>
2020-04-02 12:31:49 -0400

    pushing home view controller for alex to take a look at.

--- ReddOS/HomeViewController.swift
+++ ReddOS/HomeViewController.swift
@@ -17,31 +17,32 @@ class HomeViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         //TODO: steps for getting user front data
         do{
             try
                 //call the front and vote endpoint
-                delegate.reddit?.loadUserFront(completionHandler: completionHandler(data:error:))
+                delegate.reddit?.loadUserFront(completionHandler:completionHandler(data:error:))
+            
         }catch{
-            
-            //leave empty for now
-            
+            print(error.localizedDescription)
             }
         }
     
     
     //take data optional and error otional
-    func completionHandler(data: [String:Any]?, error: Error?) -> Void{
+    func completionHandler(data: [Submission]?, error: Error?) -> Void{
         //check if data is legit
-        guard let data = data, error == nil else {
+        print("testing call")
+        guard let submissionList = data, error == nil else {
             fatalError()
         }
+        tableView.reloadData()
+        print(submissionList)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        tableView.reloadData()
+        //tableView.reloadData()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
@@ -52,7 +53,7 @@ class HomeViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "hotThread", for: indexPath)
         let submisson = hotSubmissions[indexPath.row]
         cell.textLabel!.text = submisson.title
-        cell.textLabel!.text = submisson.parentSubreddit.displayName
+        cell.textLabel!.text = submisson.parentSubredditName
         //cell.textLabel?.text = submisson.subreddit
         return cell
     }
